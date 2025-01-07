c97c6bc39961af54de43b8211d83cd0100a3f943
doas root <petr.zakharov.anton@gmail.com>
2025-01-07 23:27:51 +0300

    fix(rooms,python): import from proto package instead of rooms package

--- proto/rooms/python/proto/src/proto/rooms_pb2_grpc.py
+++ proto/rooms/python/proto/src/proto/rooms_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from rooms import rooms_pb2 as rooms_dot_rooms__pb2
+from proto import rooms_pb2 as rooms_dot_rooms__pb2
 
 GRPC_GENERATED_VERSION = '1.69.0'
 GRPC_VERSION = grpc.__version__
